12a7feb3437327fa89ee9982acf4712469fc3de9
ale sierra <malesierra15@gmail.com>
2025-01-28 07:50:21 -0600

    Atributos y override de istack

--- PilaVector.java
+++ PilaVector.java
@@ -1,1 +1,44 @@
+public class PilaVector implements IStack {
+    private int[] elementos;
+    private int capacidad;
+    private int tamano;
+
+    public PilaVector() {
+        capacidad  = 0;
+        elementos = new int[capacidad];
+        tamano = 10;
+    }
+
+    @Override
+    public void push(int value) {
+        if (tamano == capacidad) {
+            resize();
+        } else {
+            elementos [tamano++] = value;
+        }
+    }
+
+    @Override
+    public int pop() {
+        if (tamano == 0){
+            throw new IllegalStateException("Pila vacía");
+        }
+        return elementos[--tamano];
+    }
+
+    @Override
+    public int operation(char operator, int value1, int value2) {
+        return switch (operator) {
+            case '+' -> value1 + value2;
+            case '-' -> value1 - value2;
+            case '*' -> value1 * value2;
+            case '/' -> value1 / value2;
+            case '%' -> value1 % value2;
+
+        }
+    }
+
+}
+
+
 
