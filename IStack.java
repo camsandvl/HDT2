972e0aaa0a4af4307a012210749da68dd0b85a47
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-01-29 09:28:37 -0600

    Update IStack.java

--- IStack.java
+++ IStack.java
@@ -1,1 +1,6 @@
+public interface IStack {
+    void push(int value);                      // Add valor a la pila
+    int pop();                                // Eliminar y devolver el valor desde arriba
+    int operation(char operator, int value1, int value2); 
+}
 
